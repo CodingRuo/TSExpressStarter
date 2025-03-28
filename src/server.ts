124a85ba165fea3463cd83e3ba7cb38317ec51b5
CodingRuo <r.schober@outlook.com>
2025-03-28 06:46:04 +0100

    Adding Routing

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express, { type Request, type Response } from "express";
 import morgan from "morgan";
 import cors from "cors";
 import config from "./config";
+import v1 from "./routes/v1";
 
 export const bootStrap = () => {
   const app = express();
@@ -15,6 +16,8 @@ export const bootStrap = () => {
   app.get("/health", (_: Request, res: Response) => {
     res.json({ ok: true, env: process.env.PORT });
   });
+
+  app.use("/v1", v1);
 
   if (config.env !== "production") {
     console.clear();
